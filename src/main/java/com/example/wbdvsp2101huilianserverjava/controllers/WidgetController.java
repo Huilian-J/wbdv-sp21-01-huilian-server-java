efd30fe90fbd4326ed77d6cfd2fd69ae8dba1055
jade <hanyidaxia@gmail.com>
2021-03-21 22:53:51 -0400

    added lesson service

---
+++ src/main/java/com/example/wbdvsp2101huilianserverjava/controllers/WidgetController.java
@@ -0,0 +1,27 @@
+package com.example.wbdvsp2101huilianserverjava.controllers;
+
+import com.example.wbdvsp2101huilianserverjava.models.Widget;
+import com.example.wbdvsp2101huilianserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WidgetController {
+    @Autowired
+    WidgetService service;
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+    @GetMapping("api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(
+            @PathVariable("tid") String topicId
+    ){
+        return service.findWidgetsForTopic(topicId);
+    }
+}
