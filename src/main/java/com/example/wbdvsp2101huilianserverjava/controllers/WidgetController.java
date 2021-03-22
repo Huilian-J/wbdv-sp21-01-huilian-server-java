2d4009cc9e6271df0786c739c25894f601d2e321
jade <hanyidaxia@gmail.com>
2021-03-21 23:01:18 -0400

    cross origin

--- src/main/java/com/example/wbdvsp2101huilianserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp2101huilianserverjava/controllers/WidgetController.java
@@ -3,6 +3,7 @@ package com.example.wbdvsp2101huilianserverjava.controllers;
 import com.example.wbdvsp2101huilianserverjava.models.Widget;
 import com.example.wbdvsp2101huilianserverjava.services.WidgetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WidgetController {
     @Autowired
     WidgetService service;
